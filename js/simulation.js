62c12bd7f6ec7afe94033acd203c4fccd835742d
zorpaa <alexanderbtew2@gmail.com>
2026-07-21 03:51:58 -0700

    Refactor ride completion logic and time advancement

--- js/simulation.js
+++ js/simulation.js
@@ -1,52 +1,36 @@
 // Simulation System
 
 function advanceTime(minutes){
+  if(isNaN(minutes))return;
   parkTime.current+=minutes;
+  updateRideWaits();
   updateClock();
 }
-
 function arriveAtDestination(){
-
   if(!player.destination)return;
-
   const destination=player.destination;
-
   console.log("Arrived:",nodes[destination].name);
-
   player.currentNode=destination;
-
   if(nodes[destination].type==="ride"){
     completeRide(destination);
   }
-
   player.destination=null;
   player.path=[];
   player.pathIndex=0;
   player.moving=false;
 }
-
-
 function completeRide(id){
-
   const ride=rides[id];
-
   if(!ride||ride.completed)return;
-
   if(parkIsClosed()){
     console.log("Entered queue before closing.");
   }
-
-  advanceTime(ride.wait);
+  advanceTime(ride.currentWait);
   advanceTime(ride.duration);
-
   ride.completed=true;
-
   player.completed.push(id);
-
   updateRideCounter();
-
   console.log(ride.name+" completed!");
-
   if(checkWin()){
     victory();
   }
@@ -55,43 +39,28 @@ function completeRide(id){
 
 
 function allRidesComplete(){
-
   for(let id in rides){
     if(!rides[id].completed)return false;
   }
-
   return true;
-
 }
 
 
 function checkParkStatus(){
-
   if(parkIsClosed()){
-
     alert("Park is closed!");
-
     return false;
-
   }
-
   return true;
-
 }
-
-
 function updateRideCounter(){
-
   const counter=document.getElementById("rides");
-
   if(!counter)return;
-
   counter.innerText=
     "Rides: "+
     player.completed.length+
     " / "+
     Object.keys(rides).length;
-
 }
 function checkWin(){
   return allRidesComplete();
@@ -101,13 +70,10 @@ function victory(){
   alert("Congratulations! You completed all rides!");
 }
 function updateHUD(){
-
   let loc=document.getElementById("location");
   let status=document.getElementById("status");
-
   if(loc)
     loc.innerText="Location: "+nodes[player.currentNode].name;
-
   if(status)
     status.innerText=parkIsClosed()?"Park Closed":"Park Open";
 }
