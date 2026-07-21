0a3a8522aa082055defb4854043de87754056402
zorpaa <alexanderbtew2@gmail.com>
2026-07-20 17:00:21 -0700

    Create rides.js

---
+++ js/rides.js
@@ -0,0 +1,49 @@
+// ====================
+// Ride Database
+// ====================
+
+const rides = {
+
+  peterPan: {
+    name: "Peter Pan's Flight",
+    wait: 45,
+    duration: 3,
+    completed: false
+  },
+
+  matterhorn: {
+    name: "Matterhorn Bobsleds",
+    wait: 40,
+    duration: 4,
+    completed: false
+  },
+
+  spaceMountain: {
+    name: "Space Mountain",
+    wait: 50,
+    duration: 5,
+    completed: false
+  },
+
+  indianaJones: {
+    name: "Indiana Jones Adventure",
+    wait: 45,
+    duration: 8,
+    completed: false
+  },
+
+  bigThunder: {
+    name: "Big Thunder Mountain",
+    wait: 35,
+    duration: 4,
+    completed: false
+  },
+
+  rise: {
+    name: "Rise of the Resistance",
+    wait: 70,
+    duration: 18,
+    completed: false
+  }
+
+};
