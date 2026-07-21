7cfc48df00aa9fa6d9acd94168980d61a5dd17b0
zorpaa <alexanderbtew2@gmail.com>
2026-07-20 17:37:07 -0700

    Update ui.js
    An error required an overhaul of ui.js

--- js/ui.js
+++ js/ui.js
@@ -3,58 +3,39 @@
 // ====================
 
 function drawConnections() {
-
   ctx.strokeStyle = "#888";
   ctx.lineWidth = 3;
 
   for (const id in nodes) {
-
     const node = nodes[id];
 
     for (const connection of node.connections) {
-
       const target = nodes[connection.node];
 
       ctx.beginPath();
-
       ctx.moveTo(node.x, node.y);
-
-      ctx.lineTo(
-        target.x,
-        target.y
-      );
-
+      ctx.lineTo(target.x, target.y);
       ctx.stroke();
-
     }
-
   }
-
 }
 
 
 function drawNodes() {
-
   for (const id in nodes) {
-
     const node = nodes[id];
 
-
     let color = "#4caf50";
-
 
     if (node.type === "ride") {
       color = "#e91e63";
     }
 
-
     if (node.type === "junction") {
       color = "#2196f3";
     }
 
-
     ctx.beginPath();
-
     ctx.arc(
       node.x,
       node.y,
@@ -64,14 +45,10 @@ function drawNodes() {
     );
 
     ctx.fillStyle = color;
-
     ctx.fill();
 
-
     ctx.fillStyle = "#000";
-
     ctx.font = "14px Arial";
-
     ctx.textAlign = "center";
 
     ctx.fillText(
@@ -79,16 +56,11 @@ function drawNodes() {
       node.x,
       node.y - 25
     );
-
   }
-
 }
 
 
 function drawParkMap() {
-
   drawConnections();
-
   drawNodes();
-
 }
