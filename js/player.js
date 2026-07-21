6cfabf4295a3fa1edc249da8c9b6ff3c52e83ef7
zorpaa <alexanderbtew2@gmail.com>
2026-07-21 03:19:41 -0700

    Refactor Player movement logic and clean up code
    Refactor Player class to improve movement logic and remove unnecessary whitespace.

--- js/player.js
+++ js/player.js
@@ -1,7 +1,5 @@
 // Player System
-
 class Player{
-
 constructor(){
   this.currentNode="entrance";
   this.x=nodes.entrance.x;
@@ -14,22 +12,15 @@ constructor(){
   this.destination=null;
 }
 
-
 startMovement(path){
-
   if(!path||path.length<2)return;
-
   this.path=path;
   this.pathIndex=1;
   this.moving=true;
-
   let distance=0;
-
   for(let i=0;i<path.length-1;i++){
-
     let a=nodes[path[i]];
     let b=nodes[path[i+1]];
-
     distance+=Math.sqrt(
       (b.x-a.x)**2+
       (b.y-a.y)**2
@@ -42,58 +33,35 @@ startMovement(path){
   addMinutes(minutes);
 }
 
-
 update(){
-
   if(!this.moving)return;
-
-
   if(this.pathIndex>=this.path.length){
-
     this.moving=false;
     arriveAtDestination();
     return;
-
   }
-
-
   let nextID=this.path[this.pathIndex];
   let next=nodes[nextID];
-
-
   if(!next){
-
     console.error("Missing node:",nextID);
     this.moving=false;
     return;
-
   }
-
-
   let dx=next.x-this.x;
   let dy=next.y-this.y;
   let distance=Math.sqrt(dx*dx+dy*dy);
-
-
   if(distance<=this.speed/60){
-
     this.x=next.x;
     this.y=next.y;
     this.currentNode=nextID;
     this.pathIndex++;
     return;
-
   }
-
-
   this.x+=(dx/distance)*(this.speed/60);
   this.y+=(dy/distance)*(this.speed/60);
-
 }
 
 }
-
-
 const player=new Player();
 function arriveAtDestination(){
   let node=nodes[player.currentNode];
