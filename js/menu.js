e8494035be8a3e70c18d61f35398de5f1b5b107f
zorpaa <alexanderbtew2@gmail.com>
2026-07-21 10:58:41 -0700

    Implement crowd forecast updates in menu.js
    Add crowd forecast functionality based on crowd size.

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,38 @@
+const crowd=document.getElementById("crowd");
+const forecast=document.getElementById("forecast");
+
+function updateForecast(){
+
+  let text="";
+
+  switch(crowd.value){
+
+    case ".75":
+      text="Today's Forecast<br><br>Quiet Crowd Day";
+      break;
+
+    case "1":
+      text="Today's Forecast<br><br>Average Crowd Day";
+      break;
+
+    case "1.25":
+      text="Today's Forecast<br><br>Busy Crowd Day";
+      break;
+
+    case "1.5":
+      text="Today's Forecast<br><br>Holiday Crowd";
+      break;
+
+  }
+
+  forecast.innerHTML=text;
+
+}
+
+crowd.onchange=updateForecast;
+
+updateForecast();
+
 document.getElementById("startGame").onclick=()=>{
 
   sessionStorage.setItem(
