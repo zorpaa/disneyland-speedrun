35616f897a9b853b4c028b05d0c3fff8c73b7510
zorpaa <alexanderbtew2@gmail.com>
2026-07-23 15:09:58 -0700

    Simplify time string by removing seconds
    Removed seconds from time string formatting.

--- js/gametime.js
+++ js/gametime.js
@@ -16,19 +16,12 @@ function getTimeString(){
   let total=parkTime.current;
   let hours=Math.floor(total/60);
   let minutes=Math.floor(total%60);
-  let seconds=Math.floor((total%1)*60);
   let period=hours>=12?"PM":"AM";
+
   if(hours>12)hours-=12;
   if(hours===0)hours=12;
-  return (
-    hours+
-    ":"+
-    String(minutes).padStart(2,"0")+
-    ":"+
-    String(seconds).padStart(2,"0")+
-    " "+
-    period
-  );
+
+  return hours+":"+String(minutes).padStart(2,"0")+" "+period;
 }
 function parkIsClosed(){
   return parkTime.current>=parkTime.close;
