7d4dbb5fef7fc3055106d85259b09559c9693091
zorpaa <alexanderbtew2@gmail.com>
2026-07-23 22:57:40 -0700

    Set coordinates for Jolly Holiday food item

--- js/graph_food.js
+++ js/graph_food.js
@@ -3,8 +3,8 @@ const foodNodes={
 jollyHoliday:{
   name:"Jolly Holiday",
   type:"food",
-  x:/* coordinate */,
-  y:/* coordinate */,
+  x:1580,
+  y:1860,
   land:"Main Street",
   showLabel:true,
 }
