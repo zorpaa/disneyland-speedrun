7f889c792cc2d6e22a5143456e4aed4f0a98d910
zorpaa <alexanderbtew2@gmail.com>
2026-08-07 20:40:54 -0700

    Add new food venues with attributes

--- js/food.js
+++ js/food.js
@@ -8,6 +8,87 @@ jollyHoliday:{
   foodRestore:60,
   happinessRestore:15,
   fatigueRestore:10
-}
+},
+
+    plazaInn:{
+    name:"Plaza Inn",
+    duration:30,
+    foodRestore:70,
+    happinessRestore:20,
+    fatigueRestore:10
+  },
+
+  galacticGrill:{
+    name:"Galactic Grill",
+    duration:20,
+    foodRestore:55,
+    happinessRestore:15,
+    fatigueRestore:8
+  },
+
+  alienPizzaPlanet:{
+    name:"Alien Pizza Planet",
+    duration:20,
+    foodRestore:60,
+    happinessRestore:15,
+    fatigueRestore:8
+  },
+
+  redRoseTaverne:{
+    name:"Red Rose Taverne",
+    duration:20,
+    foodRestore:55,
+    happinessRestore:15,
+    fatigueRestore:8
+  },
+
+  bengalBarbecue:{
+    name:"Bengal Barbecue",
+    duration:15,
+    foodRestore:45,
+    happinessRestore:12,
+    fatigueRestore:6
+  },
+
+  ranchoZocalo:{
+    name:"Rancho del Zocalo",
+    duration:25,
+    foodRestore:65,
+    happinessRestore:18,
+    fatigueRestore:10
+  },
+
+  tianasPalace:{
+    name:"Tiana's Palace",
+    duration:20,
+    foodRestore:60,
+    happinessRestore:18,
+    fatigueRestore:8
+  },
+
+  hungryBear:{
+    name:"Hungry Bear Barbeque Jamboree",
+    duration:25,
+    foodRestore:65,
+    happinessRestore:18,
+    fatigueRestore:10
+  },
+
+  cafeDaisy:{
+    name:"Cafe Daisy",
+    duration:20,
+    foodRestore:55,
+    happinessRestore:15,
+    fatigueRestore:8
+  },
+
+  rontoRoasters:{
+    name:"Ronto Roasters",
+    duration:15,
+    foodRestore:45,
+    happinessRestore:12,
+    fatigueRestore:6
+  }
+
 
 };
