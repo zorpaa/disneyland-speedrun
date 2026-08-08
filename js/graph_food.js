a45e52870735b88c111a43d2b5d4eb87609e8711
zorpaa <alexanderbtew2@gmail.com>
2026-08-07 20:40:28 -0700

    Add food nodes for various lands in the park

--- js/graph_food.js
+++ js/graph_food.js
@@ -1,12 +1,130 @@
 const foodNodes={
 
-jollyHoliday:{
+  // ====================
+  // Main Street
+  // ====================
+
+  jollyHoliday:{
   name:"Jolly Holiday",
   type:"food",
   x:1580,
   y:1860,
   land:"Main Street",
   showLabel:true,
-}
+},
+  
+  plazaInn:{
+    name:"Plaza Inn",
+    type:"food",
+    x:1850,
+    y:1830,
+    land:"Main Street",
+    showLabel:true
+  },
 
+  // ====================
+  // Tomorrowland
+  // ====================
+
+  galacticGrill:{
+    name:"Galactic Grill",
+    type:"food",
+    x:2320,
+    y:1520,
+    land:"Tomorrowland",
+    showLabel:true
+  },
+
+  alienPizzaPlanet:{
+    name:"Alien Pizza Planet",
+    type:"food",
+    x:2440,
+    y:1840,
+    land:"Tomorrowland",
+    showLabel:true
+  },
+
+  // ====================
+  // Fantasyland
+  // ====================
+
+  redRoseTaverne:{
+    name:"Red Rose Taverne",
+    type:"food",
+    x:1590,
+    y:1120,
+    land:"Fantasyland",
+    showLabel:true
+  },
+
+  cafeDaisy:{
+    name:"Cafe Daisy",
+    type:"food",
+    x:1790,
+    y:270,
+    land:"Fantasyland",
+    showLabel:true
+  },
+
+  // ====================
+  // Adventureland
+  // ====================
+
+  bengalBarbecue:{
+    name:"Bengal Barbecue",
+    type:"food",
+    x:1240,
+    y:1870,
+    land:"Adventureland",
+    showLabel:true
+  },
+
+  // ====================
+  // Frontierland
+  // ====================
+
+  ranchoZocalo:{
+    name:"Rancho del Zocalo",
+    type:"food",
+    x:1370,
+    y:1500,
+    land:"Frontierland",
+    showLabel:true
+  },
+
+  // ====================
+  // Bayou Country
+  // ====================
+
+  tianasPalace:{
+    name:"Tiana's Palace",
+    type:"food",
+    x:850,
+    y:2000,
+    land:"Bayou Country",
+    showLabel:true
+  },
+
+  hungryBear:{
+    name:"Hungry Bear Barbeque Jamboree",
+    type:"food",
+    x:480,
+    y:1410,
+    land:"Bayou Country",
+    showLabel:true
+  },
+
+  // ====================
+  // Galaxy's Edge
+  // ====================
+
+  rontoRoasters:{
+    name:"Ronto Roasters",
+    type:"food",
+    x:850,
+    y:700,
+    land:"Galaxy's Edge",
+    showLabel:true
+  }
+  
 };
